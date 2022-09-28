cd49137d8534c457875d13584255c993fbbb71a9
Ahmed Adel <dolagamer2002@gmail.com>
2022-09-28 16:02:08 +0200

    sign up screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/resources/auth_methods.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import '../widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _bioController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _usernameController.dispose();
+    _bioController.dispose();
   }
 
   @override
@@ -32,13 +37,41 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Expanded(child: Container(),),
+            Flexible(
+              flex: 2,
+              child: Container(),
+            ),
             SvgPicture.asset(
               "assets/ic_instagram.svg",
               height: 64,
               color: primaryColor,
             ),
-            const SizedBox(height: 64),
+            const SizedBox(height: 32),
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://learn.microsoft.com/en-us/answers/storage/attachments/209536-360-f-364211147-1qglvxv1tcq0ohz3fawufrtonzz8nq3e.jpg"),
+                  radius: 64,
+                ),
+                Positioned(
+                  bottom: -5,
+                  right: -5,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            TextFieldInput(
+              controller: _usernameController,
+              hintText: "Enter your username",
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 24),
             TextFieldInput(
               controller: _emailController,
               hintText: "Enter your email",
@@ -52,16 +85,32 @@ class _LoginScreenState extends State<LoginScreen> {
               isPassword: true,
             ),
             const SizedBox(height: 24),
+            TextFieldInput(
+              controller: _bioController,
+              hintText: "Enter your bio",
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 24),
             Container(
               width: double.infinity,
               child: MaterialButton(
-                onPressed: () {},
+                onPressed: () async {
+                  String res = await AuthMethods().signUpUser(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      username: _usernameController.text,
+                      bio: _bioController.text);
+                  print(res);
+                },
                 color: Colors.blue,
-                child: const Text("Log in"),
+                child: const Text("Sign up"),
                 padding: const EdgeInsets.symmetric(vertical: 12),
               ),
             ),
-            Expanded(child: Container(),),
+            Flexible(
+              flex: 2,
+              child: Container(),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -70,7 +119,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: const Text("Don't have an account? "),
                 ),
                 GestureDetector(
-                  onTap: (){},
+                  onTap: () {},
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     child: const Text("Sing up"),
